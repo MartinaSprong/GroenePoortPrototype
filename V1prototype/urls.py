75ff2a29b762386868f98e6fe964d4f3e996370d
Martina Sprong <martinasprong@hotmail.com>
2017-05-30 11:06:07 +0200

    Link to graph page with variable

--- V1prototype/urls.py
+++ V1prototype/urls.py
@@ -6,7 +6,8 @@ from django.conf.urls.static import static
 urlpatterns = (
     url(r'^$', views.index, name='index'),
     # url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
-    url(r'^graph/$', views.graphView, name='graph'),
+    url(r'^tide/(?P<oid>[0-9]+)/$', views.graphView, name='graph'), 
+    # url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+    # url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
     url(r'^tide/$', views.jsonGetTide, name='jsonGetTide')
 )
-
--- V1prototype/views.py
+++ V1prototype/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from V1prototype.models import tide 
+from V1prototype.models import tide
 from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -20,6 +20,7 @@ logging.basicConfig(level=logging.DEBUG)
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger(__name__)
 
+
 def utmToLatLng(zone, easting, northing, northernHemisphere=True):
     if not northernHemisphere:
         northing = 10000000 - northing
@@ -30,20 +31,24 @@ def utmToLatLng(zone, easting, northing, northernHemisphere=True):
     k0 = 0.9996
 
     arc = northing / k0
-    mu = arc / (a * (1 - math.pow(e, 2) / 4.0 - 3 * math.pow(e, 4) / 64.0 - 5 * math.pow(e, 6) / 256.0))
+    mu = arc / (a * (1 - math.pow(e, 2) / 4.0 - 3 *
+                     math.pow(e, 4) / 64.0 - 5 * math.pow(e, 6) / 256.0))
 
-    ei = (1 - math.pow((1 - e * e), (1 / 2.0))) / (1 + math.pow((1 - e * e), (1 / 2.0)))
+    ei = (1 - math.pow((1 - e * e), (1 / 2.0))) / \
+        (1 + math.pow((1 - e * e), (1 / 2.0)))
 
     ca = 3 * ei / 2 - 27 * math.pow(ei, 3) / 32.0
 
     cb = 21 * math.pow(ei, 2) / 16 - 55 * math.pow(ei, 4) / 32
     cc = 151 * math.pow(ei, 3) / 96
     cd = 1097 * math.pow(ei, 4) / 512
-    phi1 = mu + ca * math.sin(2 * mu) + cb * math.sin(4 * mu) + cc * math.sin(6 * mu) + cd * math.sin(8 * mu)
+    phi1 = mu + ca * math.sin(2 * mu) + cb * math.sin(4 * mu) + \
+        cc * math.sin(6 * mu) + cd * math.sin(8 * mu)
 
     n0 = a / math.pow((1 - math.pow((e * math.sin(phi1)), 2)), (1 / 2.0))
 
-    r0 = a * (1 - e * e) / math.pow((1 - math.pow((e * math.sin(phi1)), 2)), (3 / 2.0))
+    r0 = a * (1 - e * e) / \
+        math.pow((1 - math.pow((e * math.sin(phi1)), 2)), (3 / 2.0))
     fact1 = n0 * math.tan(phi1) / r0
 
     _a1 = 500000 - easting
@@ -52,13 +57,16 @@ def utmToLatLng(zone, easting, northing, northernHemisphere=True):
 
     t0 = math.pow(math.tan(phi1), 2)
     Q0 = e1sq * math.pow(math.cos(phi1), 2)
-    fact3 = (5 + 3 * t0 + 10 * Q0 - 4 * Q0 * Q0 - 9 * e1sq) * math.pow(dd0, 4) / 24
+    fact3 = (5 + 3 * t0 + 10 * Q0 - 4 * Q0 * Q0 -
+             9 * e1sq) * math.pow(dd0, 4) / 24
 
-    fact4 = (61 + 90 * t0 + 298 * Q0 + 45 * t0 * t0 - 252 * e1sq - 3 * Q0 * Q0) * math.pow(dd0, 6) / 720
+    fact4 = (61 + 90 * t0 + 298 * Q0 + 45 * t0 * t0 - 252 *
+             e1sq - 3 * Q0 * Q0) * math.pow(dd0, 6) / 720
 
     lof1 = _a1 / (n0 * k0)
     lof2 = (1 + 2 * t0 + Q0) * math.pow(dd0, 3) / 6.0
-    lof3 = (5 - 2 * Q0 + 28 * t0 - 3 * math.pow(Q0, 2) + 8 * e1sq + 24 * math.pow(t0, 2)) * math.pow(dd0, 5) / 120
+    lof3 = (5 - 2 * Q0 + 28 * t0 - 3 * math.pow(Q0, 2) + 8 *
+            e1sq + 24 * math.pow(t0, 2)) * math.pow(dd0, 5) / 120
     _a2 = (lof1 - lof2 + lof3) / math.cos(phi1)
     _a3 = _a2 * 180 / math.pi
 
@@ -71,39 +79,45 @@ def utmToLatLng(zone, easting, northing, northernHemisphere=True):
 
     return (latitude, longitude)
 
+
 def index(request):
-    #tide.objects.filter(locationName="Eemshaven")
-    #tide.objects.order_by('id')
+    # tide.objects.filter(locationName="Eemshaven")
+    # tide.objects.order_by('id')
     context = dict()
     context['tides'] = tide.objects.all()
-    return render(request,'V1prototype/index.html', context)
+    return render(request, 'V1prototype/index.html', context)
 
-def graphView(request):
-    return render(request, 'V1prototype/graph.html')
-    
+
+def graphView(request, oid):
+    print(oid)
+    tideView = tide.objects.filter(id=oid).first()
+    context = {
+        'tides': tideView
+    }
+    return render(request, 'V1prototype/graph.html', context)
+
 def jsonGetTide(object):
     #  download and de-serialize json
-     tideUrl = json.load(urllib2.urlopen('https://waterinfo.rws.nl/api/point/latestmeasurements?parameterid=waterhoogte-t-o-v-nap '))
-     tideValues = tideUrl['features']
-     for tideValue in tideValues:
-         tideMeasurements = tideValue['properties']['measurements']
-         convertCoordinates = utmToLatLng(31, tideValue['geometry']['coordinates'][0], 
-                                            tideValue['geometry']['coordinates'][1], northernHemisphere=True)
-         for tideValueSecond in tideMeasurements:
-             latestValue = tideValueSecond['dateTime']
+    tideUrl = json.load(urllib2.urlopen(
+        'https://waterinfo.rws.nl/api/point/latestmeasurements?parameterid=waterhoogte-t-o-v-nap '))
+    tideValues = tideUrl['features']
+    for tideValue in tideValues:
+        tideMeasurements = tideValue['properties']['measurements']
+        convertCoordinates = utmToLatLng(31, tideValue['geometry']['coordinates'][0],
+                                         tideValue['geometry']['coordinates'][1],
+                                         northernHemisphere=True)
+        for tideValueSecond in tideMeasurements:
+            latestValue = tideValueSecond['dateTime']
     #      correctTime = (datetime.datetime.fromtimestamp(int(tideTime)).strftime('%Y-%m-%d %H:%M:%S'))
-             t1 = tide.objects.update_or_create(parameterName=tideValueSecond['parameterId'], 
-                                                    value=tideValueSecond['latestValue'], 
-                                                    unit=tideValueSecond['unitCode'],
-                                                    time=tideValueSecond['dateTime'], 
-                                                    locationName=tideValue['properties']['name'],
-                                                    lat=convertCoordinates[0],
-                                                    lon=convertCoordinates[1])
-     log.debug("Entering debug mode")
-     log.info("Hey there it works!!")
-     log.debug("Checking for git user") 
-     print(t1)  
-     return HttpResponse()
-
-
-    
+            t1 = tide.objects.update_or_create(parameterName=tideValueSecond['parameterId'],
+                                               value=tideValueSecond['latestValue'],
+                                               unit=tideValueSecond['unitCode'],
+                                               time=tideValueSecond['dateTime'],
+                                               locationName=tideValue['properties']['name'],
+                                               lat=convertCoordinates[0],
+                                               lon=convertCoordinates[1])
+    log.debug("Entering debug mode")
+    log.info("Hey there it works!!")
+    log.debug("Checking for git user")
+    print t1
+    return HttpResponse()
